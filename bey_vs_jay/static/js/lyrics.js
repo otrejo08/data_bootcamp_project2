b429d6caa7d07f09289954f6d75c4c985c178c6f
jormax <43761101+jormax14@users.noreply.github.com>
2019-04-11 08:38:40 -0500

    fixing js

--- bey_vs_jay/static/js/lyrics.js
+++ bey_vs_jay/static/js/lyrics.js
@@ -1,34 +1,33 @@
-function buildBubble(lyric) {
-    d3.json(`/lyrics/${lyrics}`).then(function(lyricData) {
-        var releaseDate = lyricData.release_data;
-        var uniqueWords = lyricData.percent_unique;
-        var wordCount = lyricData.word_count;
-        var song = lyricData.song;
-        var song_url = lyricData.song_url
+var url = '/lyrics';
+d3.json(url).then(function(lyricData) {
+    var releaseDate = lyricData.release_data;
+    var uniqueWords = lyricData.percent_unique;
+    var wordCount = lyricData.word_count;
+    var song = lyricData.song;
+    var song_url = lyricData.song_url
 
-        var BubbleTrace = {
-            x: releaseDate,
-            y: uniqueWords,
-            text: song,
-            mode: 'markers',
-            marker: {
-                size: wordCount,
-                colorscale: 'blue'
-            }
-        };
-        var bubbleData = [bubbleTrace];
-        var bubbleLayout = {
-            xaxis: {
-                showlegend: false,
-                title: {
-                    text: 'years',
-                    font: {
-                        size: 20
-                    }
+    var BubbleTrace = {
+        x: releaseDate,
+        y: uniqueWords,
+        text: song,
+        mode: 'markers',
+        marker: {
+            size: wordCount,
+            colorscale: 'blue'
+        }
+    };
+    var bubbleData = [bubbleTrace];
+    var bubbleLayout = {
+        xaxis: {
+            showlegend: false,
+            title: {
+                text: 'years',
+                font: {
+                    size: 20
                 }
             }
-        };
+        }
+    };
 
-        Plotly.newPlot('bubble', bubbleData, bubbleLayout, {response: true});
-    });
-}
+    Plotly.Plot('bubble', bubbleData, bubbleLayout, {response: true});
+});
